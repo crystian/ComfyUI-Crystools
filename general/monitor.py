9e39a111a50887345728f181e82eb61ddf29defb
Crystian <crystian@gmail.com>
2024-01-07 15:36:00 +0100

    server created

--- general/monitor.py
+++ general/monitor.py
@@ -1,4 +1,3 @@
-import os
 import asyncio
 import server
 import time
@@ -7,11 +6,14 @@ import psutil
 import torch
 import pynvml
 from comfy.model_management import get_torch_device_name, get_torch_device
+from server import PromptServer
+from aiohttp import web
 from ..core import logger
 
 
 class CMonitor:
     monitorThread = None
+    threadController = threading.Event()
     rate = 0
     nvidia = False
     pynvmlLoaded = False
@@ -131,16 +133,17 @@ class CMonitor:
         await s.send_json('crystools.monitor', data)
 
     def monitorLoop(self):
-        while self.rate > 0:
+        while self.rate > 0 and not self.threadController.is_set():
             asyncio.run(self.send_message(self.buildMonitorData()))
             time.sleep(self.rate)
 
     def startMonitor(self):
         if self.monitorThread is not None:
-            logger.info('Restarting monitor...')
             self.stopMonitor()
+            logger.info('Restarting monitor...')
         else:
             if self.rate == 0:
+                logger.debug('Monitor rate is 0, not starting monitor.')
                 return None
 
             logger.info('Starting monitor...')
@@ -163,16 +166,91 @@ class CMonitor:
         if self.nvidia and self.cudaAvailable and self.torchDevice == 'cpu':
             logger.warn('CUDA is available, but torch is using CPU.')
 
-        monitorThread = threading.Thread(target=self.monitorLoop)
-        # TODO
-        # monitorThread.set()
-        monitorThread.daemon = True
-        monitorThread.start()
+        self.threadController.clear()
+
+        if self.monitorThread is None or not self.monitorThread.is_alive():
+            self.monitorThread = threading.Thread(target=self.monitorLoop)
+            self.monitorThread.daemon = True
+            self.monitorThread.start()
+
 
     def stopMonitor(self):
         logger.debug('Stopping monitor...')
-        # TODO
-        # monitorThread.stop()
+        self.threadController.set()
 
 
-cmonitor = CMonitor(0, False, False, False, False, False)
+cmonitor = CMonitor(1, False, False, False, False, False)
+
+
+@PromptServer.instance.routes.patch("/crystools/monitor")
+async def newSettings(request):
+    try:
+        settings = await request.json()
+
+        if 'rate' in settings is not None:
+            rate = settings['rate']
+            if type(rate) is not int and type(rate) is not float:
+                raise Exception('Rate must be an number.')
+
+            cmonitor.rate = rate
+
+        if 'switchCPU' in settings is not None:
+            switchCPU = settings['switchCPU']
+            if type(switchCPU) is not bool:
+                raise Exception('switchCPU must be an boolean.')
+
+            cmonitor.switchCPU = switchCPU
+
+        if 'switchGPU' in settings is not None:
+            switchGPU = settings['switchGPU']
+            if type(switchGPU) is not bool:
+                raise Exception('switchGPU must be an boolean.')
+
+            cmonitor.switchGPU = switchGPU
+
+        if 'switchHDD' in settings is not None:
+            switchHDD = settings['switchHDD']
+            if type(switchHDD) is not bool:
+                raise Exception('switchHDD must be an boolean.')
+
+            cmonitor.switchHDD = switchHDD
+
+        if 'switchRAM' in settings is not None:
+            switchRAM = settings['switchRAM']
+            if type(switchRAM) is not bool:
+                raise Exception('switchRAM must be an boolean.')
+
+            cmonitor.switchRAM = switchRAM
+
+        if 'switchVRAM' in settings is not None:
+            switchVRAM = settings['switchVRAM']
+            if type(switchVRAM) is not bool:
+                raise Exception('switchVRAM must be an boolean.')
+
+            cmonitor.switchVRAM = switchVRAM
+
+        return web.Response(status=200)
+    except Exception as e:
+        logger.error(e)
+        return web.Response(status=400, text=str(e))
+
+
+@PromptServer.instance.routes.post("/crystools/monitor/switch")
+async def monitorSwitch(request):
+    try:
+        switch = await request.json()
+
+        if 'monitor' in switch is not None:
+            monitor = switch['monitor']
+            if type(monitor) is not bool:
+                raise Exception('monitor must be an boolean.')
+
+            if monitor:
+                cmonitor.startMonitor()
+            else:
+                cmonitor.stopMonitor()
+
+        return web.Response(status=200)
+    except Exception as e:
+        logger.error(e)
+        return web.Response(status=400, text=str(e))
