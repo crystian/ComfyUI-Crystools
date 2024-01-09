718d3a2ca440804153fc823116be508b8f665c39
Crystian <crystian@gmail.com>
2024-01-09 03:42:19 +0100

    test

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 @author: Crystian
 @title: Crystools
 @nickname: Crystools
-@version: 1.3.3
+@version: 1.3.4-beta
 @project: "https://github.com/crystian/ComfyUI-Crystools",
 @description: Plugins for multiples uses, mainly for debugging, you need them! IG: https://www.instagram.com/crystian.ia
 """
