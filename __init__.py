b2beae62a7c921f6ec1207d3ec6c5caab1011ed7
Crystian <crystian@gmail.com>
2024-08-02 01:12:28 +0200

    HF arch

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 @author: Crystian
 @title: Crystools
 @nickname: Crystools
-@version: 1.16.5
+@version: 1.16.6
 @project: "https://github.com/crystian/ComfyUI-Crystools",
 @description: Plugins for multiples uses, mainly for debugging, you need them! IG: https://www.instagram.com/crystian.ia
 """
